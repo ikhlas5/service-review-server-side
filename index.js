a9be40c39bd571f44edabbd7bc893b4e1d023f3f
ikhlas5 <mohammadikhlas765@gmail.com>
2022-11-08 16:20:19 +0600

    three commit

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express=require('express');
 const app =express();
 const cors=require('cors')
 const port= process.env.PORt|| 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rrizfqd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const serviceCollection = client.db('weddingPhotographer').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{
+
+    }
+}
+run()
 // 1 testing purpuse
 app.get('/', (req,res)=>{
     res.send('service review running')
