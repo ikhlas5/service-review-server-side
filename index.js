6b2f0487bff99856a811ef40c825b3e195e24c24
ikhlas5 <mohammadikhlas765@gmail.com>
2022-11-09 15:11:48 +0600

    five commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express=require('express');
 const app =express();
 const cors=require('cors')
 const port= process.env.PORt|| 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
@@ -16,18 +16,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('weddingPhotographer').collection('services');
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
     }
     finally{
 
     }
 }
-run()
+run().catch(err => console.error(err));
 // 1 testing purpuse
 app.get('/', (req,res)=>{
     res.send('service review running')
